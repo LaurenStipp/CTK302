6bcd5fdf785e53b87cb4362f6d3f51a7741b67b6
Lauren <lstipp@mchsi.com>
2021-01-26 10:01:41 -0600

    code for mp1

---
+++ Unit1/mp1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255,255,255);
+  noStroke();
+
+  fill(255, 128, 147);
+  ellipse(50,70,90,30);
+  ellipse(50,95,80,30);
+  ellipse(50,115,90,30);
+
+  fill(255, 179, 191);
+  ellipse(100,100,100,120);
+}
