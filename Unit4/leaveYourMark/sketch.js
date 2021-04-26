1e70d57220c5d3eaa31e878b56a944994c9e2ea6
LaurenStipp <lstipp@mchsi.com>
2021-04-26 17:47:24 -0500

    efeeer

--- Unit4/leaveYourMark/sketch.js
+++ Unit4/leaveYourMark/sketch.js
@@ -141,14 +141,14 @@ class Bubble {
 
     switch(numberOfTouches) {
 
-      if (numberOfTouches > 1) {
-        numberOfTouches = 1;
-      }
+      // if (numberOfTouches > 1) {
+      //   numberOfTouches = 1;
+      // }
       case 0:
       text("HI",width/2,height/2);
       break;
 
-      case 1:
+      case 2:
       text("HELLO",width/2,height/2);
       break;
 
